d35bd15425dfa21415194e6617fec975bda0d002
Tony Mutai <tonnimutai@gmail.com>
2017-11-19 18:51:22 +0300

    set a default value

--- models/question.js
+++ models/question.js
@@ -13,7 +13,7 @@ var Schema = mongoose.Schema;
 
 var QuestionSchema = new Schema({       
     question_text:     { type: String, required: true },
-    remark:    { type: String },
+    remark:    { type: String, default: '' },
     type:      { type: String, enums: ['Yes/No', 'Fill In Blank', 'Multiple Choice'] },
     required:  { type: Boolean, default: false },
     options:   [{ type: String }],
